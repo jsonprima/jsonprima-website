80bf15a543a9dc2c1f21012bbdc161a0a3229e95
George Gkasdrogkas <georgegkas@gmail.com>
2019-07-01 15:46:02 +0300

    feat: implement Editor organism from mockup

--- src/scenes/App/index.js
+++ src/scenes/App/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './organisms/Header'
 import Footer from './organisms/Footer'
+import Editor from './organisms/Editor'
 import { Wrapper, Container } from './styles'
 
 const App = () => (
   <Wrapper>
     <Container width={1} py={32} as="main">
       <Header />
+      <Editor />
       <Footer />
     </Container>
   </Wrapper>
---
+++ src/scenes/App/organisms/Editor/atoms/ValidateButton/index.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+const ValidateButton = styled.button`
+  border: 0;
+  background: 0;
+  color: #4f4f4f;
+  display: inline-block;
+  padding: 12px 24px;
+  border: 1px solid #4f4f4f;
+  transition: all 0.1s ease-in;
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+  width: 100%;
+
+  :hover {
+    color: #fff;
+    background: #3bbec3;
+    border: 1px solid #3bbec3;
+  }
+
+  :focus,
+  :active,
+  :hover {
+    outline: none;
+  }
+`
+
+export default ValidateButton
