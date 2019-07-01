dc3ec6916fa8986d63684d73a6000844e63f9f58
George Gkasdrogkas <georgegkas@gmail.com>
2019-07-01 12:57:19 +0300

    feat: implement Footer organism from mockup

--- src/scenes/App/index.js
+++ src/scenes/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './organisms/Header'
+import Footer from './organisms/Footer'
 import { Wrapper, Container } from './styles'
 
 const App = () => (
   <Wrapper>
     <Container width={1} py={32}>
       <Header />
+      <Footer />
     </Container>
   </Wrapper>
 )
