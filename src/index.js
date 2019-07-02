8d9ae3218072fbc8ea5fc69692e7ca910e15219d
George Gkasdrogkas <georgegkas@gmail.com>
2019-07-02 12:20:45 +0300

    refactor: use GlobalStyle component instead of global.css file
    This change creates a more unified DX where we only write CSS in JS code.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './global.css'
+import GlobalStyle from './styles.global'
 import App from './scenes/App'
 import * as serviceWorker from './services/serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <React.Fragment>
+    <GlobalStyle />
+    <App />
+  </React.Fragment>,
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
