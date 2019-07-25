54789213d383a8ca4f92fe7cc1ac3976755e6a0e
George Gkasdrogkas <georgegkas@gmail.com>
2019-07-25 14:21:49 +0300

    refactor: move to axios library for HTTP requests

--- src/scenes/App/organisms/Editor/duck/operations.js
+++ src/scenes/App/organisms/Editor/duck/operations.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import * as actions from './actions'
 
 const { update } = actions
@@ -5,12 +6,19 @@ const { update } = actions
 const validate = code => dispatch => {
   dispatch(actions.requestValidate(code))
 
-  return fetch('https://jsonprima-api.herokuapp.com/api/' + code, {
-    method: 'post',
-  })
-    .then(res => res.json())
-    .then(response => dispatch(actions.successValidate(response)))
-    .catch(error => dispatch(actions.failureValidate(error)))
+  return axios
+    .post('https://jsonprima-api.herokuapp.com/api', {
+      code,
+    })
+    .then(response => dispatch(actions.successValidate(response.data)))
+    .catch(error =>
+      dispatch(
+        actions.failureValidate({
+          message: error.message,
+          name: error.name,
+        }),
+      ),
+    )
 }
 
 export { update, validate }
